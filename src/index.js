5cfb0b9c0102fb5b14400179413648d909027f74
yCroma <kuroyuki0728@gmail.com>
2021-09-20 01:20:55 +0900

    CHANGE: Gameを関数コンポーネントにした

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
+import Game from "./Game";
 
 ReactDOM.render(<Game />, document.getElementById("root"));
