5790012c8ef894306d3b36b8468f32ba2643b075
yCroma <kuroyuki0728@gmail.com>
2021-09-27 17:51:12 +0900

    ADD: clickでalertがでるようにした
    Reactチュートリアルの「インタラクティブなコンポーネントを作る」を実装した

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Square = ({ value }) => {
-  return <button className="square">{value}</button>;
+  return (
+    <button
+      className="square"
+      onClick={() => {
+        alert("click");
+      }}
+    >
+      {value}
+    </button>
+  );
 };
 
 export default Square;
