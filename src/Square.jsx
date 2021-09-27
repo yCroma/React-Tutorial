c2ea7aeb6be490a226b5154e2f6967442bc0b693
yCroma <kuroyuki0728@gmail.com>
2021-09-27 18:26:52 +0900

    ADD: clickするとxが表示される処理を追加

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Square = ({ index }) => {
+  const [value, setValue] = useState(null);
   return (
     <button
       className="square"
       onClick={() => {
-        alert("click");
+        setValue("x");
       }}
     >
-      {index}
+      {value}
     </button>
   );
 };
